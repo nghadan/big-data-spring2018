47581e1e25900c591ac19fb165ccd7f7c3f1149b
Nour Ghadanfar <nghadan@dhcp-18-111-55-237.dyn.mit.edu>
2018-02-20 09:49:10 -0500

    first draft of PS1

---
+++ week-02/submission/PS1.py
@@ -0,0 +1,12 @@
+# Nour Ghadanfar PS1
+
+# A. Lists
+# Create a list containing any 4 strings.
+# Print the 3rd item in the list - remember how Python indexes lists!
+# Print the 1st and 2nd item in the list using [:] index slicing.
+# Add a new string with text “last” to the end of the list and print the list.
+# Get the list length and print it.
+# Replace the last item in the list with the string “new” and print
+
+list_a = ["Hey Jude, ", "don't make it bad. ", "Take a sad song ", "and make it better"]
+print (list_a)
